e94c828e2a715062cab163835c62883f1867cb6d
RAJESH S <rajeshnaidu1957@gmail.com>
2023-01-28 13:38:44 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.models import *
+
+def display_topics(request):
+    QST=Topic.objects.all()
+    d={'topics':QST}
+    return render(request,'display_topics.html',d)
+
+
+def display_webpages(request):
+    QSW=Webpage.objects.all()
+    d={'webpages':QSW}
+    return render(request,'display_webpages.html',d)
+
+def display_access(request):
+    QSW=AccessRecords.objects.all()
+    d={'access':QSW}
+    return render(request,'display_access.html',d)
